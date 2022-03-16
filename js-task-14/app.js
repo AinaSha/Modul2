ef6348bd6689679c4013f1b3117b6042d23ea004
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2022-03-16 20:27:04 +0600

    feat: creat adaptive navbar

---
+++ js-task-14/app.js
@@ -0,0 +1,5 @@
+document.querySelector(".burger").addEventListener('click', function(){
+   document.querySelector('.burger','.menu').classList.toggle('active');
+   document.querySelector('.menu').classList.toggle('open');
+})
+
